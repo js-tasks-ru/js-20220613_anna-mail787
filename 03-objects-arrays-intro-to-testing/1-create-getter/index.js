17bc08b5d0428a37ae6cdf80506586e186f4ee36
Anna Mironovich <anna@Mir-MacBook-Pro.local>
2022-06-22 14:02:16 +0400

    03-1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return (obj) => {
+    const keys = path.split('.');
+    let newObj = {...obj};
 
+    for (let i = 0; i < keys.length; i++) {
+      newObj = newObj[keys[i]];
+      if (!newObj) {
+        return;
+      }
+    }
+    return newObj;
+  }
 }
